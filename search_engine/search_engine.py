a6efb75fc60810aa13dc40adef1dec884c6f9cac
aleksei z <farguzalexey@gmail.com>
2026-04-04 11:25:38 +0300

    feat: search works correctly with punctuation

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,10 +1,16 @@
-def search(docs:list[str], text:str) -> list[str]:
+import re
+import string
+
+def search(docs:list[dict], text:str) -> list[str]:
     search_result:list[str] = []
     if not docs:
         return search_result
 
+    clean_word:str = text.lower().strip(string.punctuation)
+
     for document in docs:
-        if text.lower() in document['text'].lower().split():
+        words:set[str] = set(re.findall(r'\w+', document['text'].lower()))
+        if clean_word in words:
             search_result.append(document['id'])
-    
+
     return search_result
--- search_engine/tests/test_search_engine.py
+++ search_engine/tests/test_search_engine.py
@@ -1,15 +1,26 @@
+import pytest
+
 from search_engine.search_engine import search
 
-def test_search_basic():
+
+@pytest.fixture
+def docs():
     doc1 = "I can't shoot straight unless I've had a pint!"
     doc2 = "Don't shoot shoot shoot that thing at me."
     doc3 = "I'm your shooter."
-
     docs = [
-        {'id': 'doc1', 'text': doc1},
-        {'id': 'doc2', 'text': doc2},
-        {'id': 'doc3', 'text': doc3},
-    ]
+            {'id': 'doc1', 'text': doc1},
+            {'id': 'doc2', 'text': doc2},
+            {'id': 'doc3', 'text': doc3},
+            ]
+    return docs
 
+
+def test_search_basic(docs):
     assert search(docs, 'shoot') == ['doc1', 'doc2']
     assert search([], 'shoot') == []
+
+
+def test_search_with_punctuation(docs):
+    assert search(docs, 'pint') == ['doc1']
+    assert search(docs, 'pint!') == ['doc1']
